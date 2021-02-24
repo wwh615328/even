09088e8d893e0c0d7c3657941efcab7fdc508371
weihua.wu <781704087@qq.com>
2021-02-24 09:18:37 +0800

    完成主页内容

--- assets/js/BaseApi.js
+++ assets/js/BaseApi.js
@@ -1,3 +1,25 @@
 $.ajaxPrefilter(function(option){
     option.url='http://ajax.frontend.itheima.net'+option.url
+    console.log(option);
+    if(option.url.indexOf('/my')){
+       option.headers= {
+            Authorization:localStorage.getItem('token')||''
+        }
+        const successFuction=option.success.bind(this)
+        option.success=function(res){
+            
+           
+            successFuction(res)
+            // option.success(res)
+            // console.log(response);
+            const {message,status} = res
+            if(message=='身份认证失败！'&&status==1){
+                localStorage.removeItem('token')
+                location.href='/login.html'
+            }
+        
+        }
+        
+    }
+    
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(function(){
+    getUserInfo()
+})
+function getUserInfo(){
+    $.ajax({
+        type: "GET",
+        url:'/my/userinfo',
+        // headers: {
+        //     Authorization:localStorage.getItem('token')
+        // }, 
+        success:function(res){
+            console.log(res);
+           if(res.status!==0){
+               
+            return layer.msg(res.message)
+           }
+         
+           renderAvatar(res.data)
+        },
+      
+    })
+    
+}
+function  renderAvatar(user){
+    const username=user.nickname||user.username
+    $('.welcome').html(username)
+    if(user.user_pic){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        const firstName=username[0].toUpperCase()
+        $('.text-avatar').html(firstName).show()
+        $('#welcome').html('欢迎'+username)
+
+    }
+}
+$('#btnLogOut').on('click',function(){
+    //eg1
+layer.confirm('是否退出', {icon: 3, title:'提示'}, function(index){
+    //do something
+    localStorage.removeItem('token')
+    location.href='/login.html'
+    layer.close(index);
+  });
+
+})
